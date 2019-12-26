65d11426bc7d5964f53197468e3d3df324a72914
Java <meena@meena-Vostro-3549>
2019-12-26 14:41:50 +0530

    code commit

---
+++ src/main/java/com/example/SpringBootLearningDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLearningDemoApplication {
+
+	public static void main(String[] args) { 
+		SpringApplication.run(SpringBootLearningDemoApplication.class, args);
+	}
+
+}
